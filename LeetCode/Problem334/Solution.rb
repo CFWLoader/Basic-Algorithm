75ba159ba17fb023977782ee3140f5feab2ccbe7
CFWLoader <cfwloader@gmail.com>
2016-02-21 16:51:57 +0800

    Copy solution for problem 334.

--- LeetCode/Problem334/Solution.rb
+++ LeetCode/Problem334/Solution.rb
@@ -2,13 +2,41 @@
 # @return {Boolean}
 def increasing_triplet(nums)
   if nums.length < 3
-    false
+    return false
   end
 
+  bound1 = nums.max
 
+  bound2 = bound1 + 1
+
+  bound1 = bound2
+
+  nums.each { |element|
+
+    # print element, '(', bound1, ',', bound2, ') '
+
+    if element <= bound1
+      bound1 = element
+    elsif element <= bound2
+      bound2 = element
+    else
+      return true
+    end
+
+  }
+
+  # puts
+
+  false
 end
 
 
 nums = [1, 2, 3]
 
-print increasing_triplet nums
+puts increasing_triplet []
+
+puts increasing_triplet [1, 5]
+
+puts increasing_triplet nums
+
+puts increasing_triplet [5, 4, 3, 2, 1]
