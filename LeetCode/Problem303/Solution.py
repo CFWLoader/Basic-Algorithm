a9c7e6781975588b68a4818a88c00b092becced6
cfwloader <cfwloader@yahoo.com>
2016-03-06 08:45:33 +0800

    Initializing problem 303.

---
+++ LeetCode/Problem303/Solution.py
@@ -0,0 +1,22 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
