99f78e71e2327b4829e4698a4a31c399af941bad
CFWLoader <cfwloader@gmail.com>
2016-03-21 20:24:44 +0800

    Sticking in problem 10.

--- LeetCode/Problem10/Solution.hpp
+++ LeetCode/Problem10/Solution.hpp
@@ -2,66 +2,83 @@
 
 using std::string;
 
-class Solution {
+class Solution
+{
 public:
-	bool isMatch(string s, string p) {
+    bool isMatch(string s, string p)
+    {
 
-		int stringIndex = 0, patternIndex = 0;
+        int stringIndex = 0, patternIndex = 0;
 
-		int stringBound = s.size(), patternBound = p.size();
+        int stringBound = s.size(), patternBound = p.size();
 
-		while(stringIndex < stringBound && patternIndex < patternBound)
-		{
+        while (stringIndex < stringBound && patternIndex < patternBound)
+        {
             // printf("stringIndex: %d, patternIndex: %d.\n", stringIndex, patternIndex);
 
-			if(s[stringIndex] == p[patternIndex] || p[patternIndex] == '.')
-			{
-				++stringIndex;
+            if (s[stringIndex] == p[patternIndex] || p[patternIndex] == '.')
+            {
+                ++stringIndex;
 
-				if((patternIndex + 1 == patternBound) || patternIndex + 1 < patternBound && p[patternIndex + 1] != '*')
-				{
-					++patternIndex;
-				}
-			}
-			else
-			{
-				++patternIndex;
+                if ((patternIndex + 1 == patternBound) || patternIndex + 1 < patternBound && p[patternIndex + 1] != '*')
+                {
+                    ++patternIndex;
+                }
+            }
+            else
+            {
+                ++patternIndex;
 
-				if((patternIndex + 1 == patternBound) || patternIndex < patternBound && p[patternIndex] == '*')
-				{
-					++patternIndex;
-				}
-			}
-		}
+                if ((patternIndex + 1 == patternBound) || patternIndex < patternBound && p[patternIndex] == '*')
+                {
+                    ++patternIndex;
+                }
+            }
+        }
 
         bool patternFinished = false;
 
-        if(patternIndex == patternBound || patternIndex + 2 == patternBound && p[patternIndex + 1] == '*')
+        if (patternIndex == patternBound || patternIndex + 2 == patternBound && p[patternIndex + 1] == '*')
         {
             patternFinished = true;
 
             patternIndex = patternBound;
         }
 
-        if(stringIndex == stringBound && !patternFinished)
+        if (stringIndex == stringBound && !patternFinished)
         {
-            while(patternIndex < patternBound)
+
+            bool softMatched = false;
+
+            if ((s[stringIndex - 1] == p[patternIndex] || p[patternIndex] == '.') && patternIndex + 1 < patternBound &&
+                p[patternIndex + 1] == '*')
             {
-                if(p[patternIndex] != '*')
+                softMatched = true;
+            }
+
+            while (patternIndex < patternBound)
+            {
+                if (p[patternIndex] != '*')
                 {
-                    if(patternIndex + 2 < patternBound && p[patternIndex + 1] == '*')
+                    if (patternIndex + 2 <= patternBound && p[patternIndex + 1] == '*')
                     {
                         patternIndex += 2;
                     }
-                    else if(s[stringIndex - 1] == p[patternIndex] || p[patternIndex] == '.')
+                    else if (s[stringIndex - 1] == p[patternIndex] || p[patternIndex] == '.')            // Here still has bug.
                     {
+                        if (softMatched)++patternIndex;
+
                         break;
                     }
                     else
                     {
                         return false;
                     }
+
+                    continue;
                 }
+
+                ++patternIndex;
             }
         }
 
