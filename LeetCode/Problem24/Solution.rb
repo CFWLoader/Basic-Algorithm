130a1c779422f7f6384c3d05a013dc5d30055c2a
CFWLoader <cfwloader@gmail.com>
2016-03-31 19:41:40 +0800

    AC problem 24.

--- LeetCode/Problem24/Solution.rb
+++ LeetCode/Problem24/Solution.rb
@@ -1,11 +1,12 @@
 # Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val)
-#         @val = val
-#         @next = nil
-#     end
-# end
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
 
 # @param {ListNode} head
 # @return {ListNode}
@@ -23,7 +24,7 @@ def swap_pairs(head)
 
   head.next = second_node
 
-  second_node.next = third_node.nil? ? third_node.next : third_node
+  second_node.next = (third_node.nil? or third_node.next.nil?) ? third_node : third_node.next
 
   until third_node.nil? do
     ptr = third_node
@@ -38,9 +39,27 @@ def swap_pairs(head)
 
     second_node.next = ptr
 
-    ptr.next = third_node.nil? ? third_node.next : third_node
+    ptr.next = (third_node.nil? or third_node.next.nil?) ? third_node : third_node.next
 
   end
 
   head
+end
+
+head = ListNode.new 1
+
+head.next = ListNode.new 2
+
+head.next.next = ListNode.new 3
+
+head.next.next.next = ListNode.new 4
+
+head = swap_pairs head
+
+ptr = head
+
+until ptr.nil?
+  print "#{ptr.val}->"
+
+  ptr = ptr.next
 end
