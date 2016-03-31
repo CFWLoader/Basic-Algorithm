6c4aa05abf3bfdc0e74c1a092e5f4885e17c5d88
CFWLoader <cfwloader@gmail.com>
2016-03-31 20:46:48 +0800

    Solving problem 12.

---
+++ LeetCode/Problem12/Solution.rb
@@ -0,0 +1,8 @@
+# @param {Integer} num
+# @return {String}
+def int_to_roman(num)
+
+  #Contruct a conversion map.
+  valueMap = {1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}
+  
+end
