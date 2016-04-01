cfb6ab44f0b75748f1ab07827d7fc695a8e4cece
cfwloader <cfwloader@yahoo.com>
2016-04-01 23:20:39 +0800

    Save before sleep.

--- LeetCode/Problem12/Solution.rb
+++ LeetCode/Problem12/Solution.rb
@@ -2,7 +2,18 @@
 # @return {String}
 def int_to_roman(num)
 
-  #Contruct a conversion map.
-  valueMap = {1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}
+  # Construct a conversion map.
+  # valueMap = {1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}
+
+  bits = Array.new 4
+
+  bits[0] = num % 10
+  num /= 10
+  bits[1] = num %10
+  num /= 10
+  bits[2] = num % 10
+  num /= 10
+  bits[3] = num % 10
+
   
 end
