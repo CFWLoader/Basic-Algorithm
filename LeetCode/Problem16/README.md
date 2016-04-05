7524b610960459006b2b29d457277140d288574c
CFWLoader <cfwloader@gmail.com>
2016-04-05 17:57:32 +0800

    Coded problem 16 solution but current state is WA.

---
+++ LeetCode/Problem16/README.md
@@ -0,0 +1,1 @@
+# Problem 16 - 3Sum Closest
---
+++ LeetCode/Problem16/Solution.rb
@@ -0,0 +1,58 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def three_sum_closest(nums, target)
+
+  if nums.nil? or nums.size < 3
+    return 0
+  end
+
+  nums.sort!
+
+  bound = nums.size
+
+  current_target = nums[0] + nums[1] + nums[2]
+
+  index = 0
+
+  until index >= bound
+
+    left = index + 1; right = bound - 1
+
+    until left >= right
+
+      assumed_target = nums[index] + nums[left] + nums[right]
+
+      if (assumed_target - target).abs < (current_target - target).abs
+        current_target = assumed_target
+      end
+
+      while left < right and nums[left] == nums[left + 1] do
+        left += 1
+      end
+      while left < right and nums[right] == nums[right - 1] do
+        right -= 1
+      end
+
+      left += 1
+      right -= 1
+
+    end
+
+    index += 1
+
+    while index > 0 and nums[index] == nums[index - 1]
+      index += 1
+    end
+
+  end
+
+  current_target
+
+end
+
+# result = three_sum [1, 3, -1, 7, 2, 6, 0, 5]
+# result = three_sum_closest [-1, 2, 1, -4], 1
+result = three_sum_closest [0, 2, 1, -3], 1
+
+print result
