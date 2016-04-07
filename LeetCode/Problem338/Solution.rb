be63d482aeee12109366353f2c3689b64eca90c2
CFWLoader <cfwloader@gmail.com>
2016-04-07 19:37:19 +0800

    AC problem 338.

--- LeetCode/Problem338/Solution.rb
+++ LeetCode/Problem338/Solution.rb
@@ -2,14 +2,43 @@
 # @return {Integer[]}
 def count_bits(num)
 
+  if num < 0
+    return nil
+  end
+
+  if num == 0
+    return [0]
+  end
+
   result = Array.new num + 1
 
   result[0] = 0
+  result[1] = 1
 
-  0.upto(num) {|val|
-    puts val
+  bit_of_two = 1
+
+  current_max_value = 2
+
+  2.upto(num) {|val|
+
+    if val >= current_max_value * 2
+
+      bit_of_two += 1
+
+      current_max_value *= 2
+
+    end
+
+    result[val] = result[val - current_max_value] + 1
+
   }
+
+  result
 
 end
 
-count_bits 3
+target = count_bits 16
+
+print target
+
+puts
