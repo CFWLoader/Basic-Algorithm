f9617333b044c0748f3135e543bd14842c78f929
CFWLoader <cfwloader@gmail.com>
2016-04-23 11:55:09 +0800

    Solving problem 22. The result algorithm generated seems like should be same as the test cases.

---
+++ LeetCode/Problem22/Solution.rb
@@ -0,0 +1,40 @@
+# @param {Integer} n
+# @return {String[]}
+def generate_parenthesis(n)
+
+  if n < 1
+    return []
+  end
+
+  if n == 1
+    return ['()']
+  end
+
+  sub_set = generate_parenthesis n - 1
+
+  result = []
+
+  sub_set.each {|element|
+
+    result << '(' + element + ')'
+
+    left_str = '()' + element
+
+    right_str = element + '()'
+
+    result << left_str
+
+    if left_str != right_str
+      result << right_str
+    end
+  }
+
+  result
+
+end
+
+result_set = generate_parenthesis 3
+
+result_set.each {|element|
+  puts element
+}
