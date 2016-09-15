faf8c703b06d9d651a21a90a6b769a15477df56b
CFWLoader <cfwloader@gmail.com>
2016-09-15 10:58:37 +0800

    Recovering Ruby Knowledges.

---
+++ LeetCode/Problem389/Solution.rb
@@ -0,0 +1,15 @@
+# @param {String} s
+# @param {String} t
+# @return {Character}
+def find_the_difference(s, t)
+
+  s.each_char { |ele| print ele }
+
+end
+
+
+s1 = 'iandsfag'
+
+t1 = 'inadg'
+
+find_the_difference s1, t1
