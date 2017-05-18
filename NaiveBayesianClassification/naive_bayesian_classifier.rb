9931540bf4c2a575ad8061070e2acd2aa3dd7c5e
CFWLoader <cfwloader@gmail.com>
2017-05-18 16:08:58 +0800

    The construction procedure of naive bayes finished.

--- NaiveBayesianClassification/naive_bayesian_classifier.rb
+++ NaiveBayesianClassification/naive_bayesian_classifier.rb
@@ -1,5 +1,3 @@
-require 'matrix'
-
 class NaiveBayesianClassifier
 
   def map_val_to_int vec
@@ -8,7 +6,7 @@ class NaiveBayesianClassifier
 
     v_idx = 0
 
-    0.upto(vec.size - 1) {|idx|
+    0.upto(vec.size - 1) { |idx|
 
       unless v_map_i.include? vec[idx]
 
@@ -24,13 +22,43 @@ class NaiveBayesianClassifier
 
   end
 
+  def build_x_map_tbls x
+
+    @x_map_tbls = Array.new(x[0].size)
+
+    @x_map_tbls.each_index {|idx| @x_map_tbls[idx] = {}}
+
+    x.each_with_index { |row, ridx|
+
+      row.each_with_index { |val, cidx|
+
+        # puts "#{cidx}, #{val}, #{@x_map_tbls[cidx].inspect}"
+
+        unless @x_map_tbls[cidx].include? val
+
+          max_idx = 0
+
+          @x_map_tbls[cidx].each_value { |idx_val|
+            max_idx += 1
+          }
+
+          @x_map_tbls[cidx][val] = max_idx
+
+        end
+
+      }
+
+    }
+
+  end
+
   def check_y y
 
     @y_map = map_val_to_int y
 
     @y_stt = Array.new @y_map.size, 0
 
-    y.each {|ele|
+    y.each { |ele|
 
       @y_stt[@y_map[ele]] += 1
 
@@ -40,7 +68,7 @@ class NaiveBayesianClassifier
 
     @y_prob = @y_map.size, 0
 
-    @y_stt.each_with_index {|val, idx|
+    @y_stt.each_with_index { |val, idx|
 
       @y_prob[idx] = val / total_size.to_f
 
@@ -50,43 +78,56 @@ class NaiveBayesianClassifier
 
   def check_x x, y
 
-    @x_map_tbls = Array.new x.column_count
+    row_size = x.size
 
-    @x_stt_tbls = Array.new x.column_count
+    column_size = x[0].size
 
-    @x_prob_tbls = Array.new x.column_count
+    # @x_map_tbls = Array.new column_size
 
-    0.upto(x.column_count - 1) {|cidx|
+    build_x_map_tbls x
 
-      x_map = map_val_to_int x.column(cidx)
+    @x_stt_tbls = Array.new column_size
 
-      @x_map_tbls[cidx] = x_map
+    @x_prob_tbls = Array.new column_size
 
-      x_stt_tbl = Matrix.build(@y_map.size, x_map.size) {0}
+    0.upto(column_size - 1) { |cidx|
 
-      0.upto(x.row_count - 1) {|ridx|
+      # x_map = map_val_to_int x[0].size
+      #
+      # @x_map_tbls[cidx] = x_map
+      x_map = @x_map_tbls[cidx]
 
-        y[ridx]
+      x_stt_tbl = Array.new(@y_map.size)
 
-        y_idx = @y_map[y[ridx]]
+      x_stt_tbl.each_index {|idx| x_stt_tbl[idx] = Array.new(x_map.size, 0)}
 
-        x_map_val = x[ridx, cidx]
+      0.upto(row_size - 1) { |ridx|
 
-        x_idx = x_map[x_map_val]
+        # y_idx = @y_map[y[ridx]]
+        #
+        # x_map_val = x[ridx][cidx]
+        #
+        # x_idx = x_map[x_map_val]
+        #
+        # x_stt_tbl[y_idx][x_idx] += 1
 
-        x_stt_tbl[y_idx, x_idx] += 1
-
-        # x_stt_tbl[@y_map[y[ridx]]][x_map[x[ridx][cidx]]] += 1
+        x_stt_tbl[@y_map[y[ridx]]][x_map[x[ridx][cidx]]] += 1
 
       }
 
       @x_stt_tbls[cidx] = x_stt_tbl
 
-      x_prob_tbl = Matrix.build(@y_map.size, x_map.size) {0}
+      x_prob_tbl = Array.new(@y_map.size)
 
-      x_stt_tbl.each_with_index {|e, row, col|
+      x_prob_tbl.each_index {|idx| x_prob_tbl[idx] = Array.new(x_map.size, 0)}
 
-        x_prob_tbl[row][col] = e/@y_stt[row].to_f
+      x_stt_tbl.each_with_index { |row_val, row|
+
+        row_val.each_with_index { |val, col|
+
+          x_prob_tbl[row][col] = val/@y_stt[row].to_f
+
+        }
 
       }
 
@@ -98,6 +139,12 @@ class NaiveBayesianClassifier
 
   def fit x, y
 
+    if x.size < 0 or y.size < 0 or x.size != y.size
+
+      raise Exception 'Empty data set or the rows of attribute dataset is not equals to target dataset.'
+
+    end
+
     check_y y
 
     check_x x, y
@@ -106,7 +153,33 @@ class NaiveBayesianClassifier
 
   def show_y
 
+    puts 'y map:'
+
+    puts @y_map.inspect
+
+    puts 'y statistic:'
+
+    puts @y_stt.inspect
+
+    puts 'y probability:'
+
     puts @y_prob.inspect
+
+  end
+
+  def show_x_ana_data
+
+    puts 'X map tables:'
+
+    puts @x_map_tbls
+
+    puts 'X statistic tables:'
+
+    puts @x_stt_tbls.inspect
+
+    puts 'X probability tables:'
+
+    puts @x_prob_tbls.inspect
 
   end
 
@@ -144,7 +217,7 @@ def load_employee_data path
 
   src_data.close
 
-  return Matrix.rows(train_collection), target_collection
+  return train_collection, target_collection
 
 end
 
@@ -158,5 +231,7 @@ if __FILE__ == $0
   classifier.fit train, target
 
   classifier.show_y
+
+  classifier.show_x_ana_data
 
 end
