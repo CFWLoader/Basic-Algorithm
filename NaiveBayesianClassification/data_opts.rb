aa9d7fc9ae3ff94ec8ac2ff23dd5b8a089048551
cfwloader <cfwloader@gmail.com>
2017-05-19 21:50:29 +0800

    generating reasonable data.

---
+++ NaiveBayesianClassification/data_opts.rb
@@ -0,0 +1,63 @@
+def regenerate_employee_data path
+
+  src_data = open path, 'r'
+
+  regened = open './employees_regened.data', 'w'
+
+  train_collection = []
+
+  target_collection = []
+
+  src_data.each { |line|
+
+    record = line.chomp.split(',')
+
+    # trans_rec = []
+    # trans_rec['department'] = record[0]
+    # trans_rec['status'] = record[1]
+    # trans_rec['age'] = record[2]
+    # trans_rec['salary'] = record[3]
+    # trans_rec['count'] = record[4]
+
+    rec_count = record[4].to_i
+
+    age_range = record[2].split('-')
+
+    age_lower_bound = age_range[0].to_i
+
+    age_upper_bound = age_range[1].to_i
+
+    salary_range = record[3].split('-')
+
+    salary_lower_bound = salary_range[0].to_i
+
+    salary_upper_bound = salary_range[1].to_i
+
+    1.upto(rec_count) {
+
+      regened.write "#{record[0]},#{Random.rand age_lower_bound..age_upper_bound},#{Random.rand salary_lower_bound..salary_upper_bound},#{record[1]}\n"
+
+      #puts "Record:[dept:#{record[0]}, age:#{Random.rand age_lower_bound..age_upper_bound}, salary:#{Random.rand salary_lower_bound..salary_upper_bound}, status:#{record[1]}]"
+
+    }
+
+    # train_collection << [record[0], record[2], record[3]]
+    #
+    # target_collection << record[1]
+
+  }
+
+  src_data.close
+
+  regened.close
+
+  return train_collection, target_collection
+
+end
+
+
+if $0 == __FILE__
+
+  regenerate_employee_data './employees.data'
+
+end
--- NaiveBayesianClassification/hybrid_bayesian_classifier.rb
+++ NaiveBayesianClassification/hybrid_bayesian_classifier.rb
@@ -1,5 +1,129 @@
 class HybridBayesianClassifier
 
+  def map_val_to_int vec
+
+    v_map_i = {}
+
+    v_idx = 0
+
+    0.upto(vec.size - 1) { |idx|
+
+      unless v_map_i.include? vec[idx]
+
+        v_map_i[vec[idx]] = v_idx
+
+        v_idx += 1
+
+      end
+
+    }
+
+    v_map_i
+
+  end
+
+  def build_x_map_tbls x, discrete_spec
+
+    @x_map_tbls = Array.new(x[0].size)
+
+    @x_map_tbls.each_index {|idx| @x_map_tbls[idx] = {}}
+
+    x.each_with_index { |row, ridx|
+
+      row.each_with_index { |val, cidx|
+
+        # puts "#{cidx}, #{val}, #{@x_map_tbls[cidx].inspect}"
+
+        unless @x_map_tbls[cidx].include? val
+
+          max_idx = 0
+
+          @x_map_tbls[cidx].each_value { |idx_val|
+            max_idx += 1
+          }
+
+          @x_map_tbls[cidx][val] = max_idx
+
+        end
+
+      }
+
+    }
+
+  end
+
+  def check_y y
+
+    @y_map = map_val_to_int y
+
+    @y_stt = Array.new @y_map.size, 0
+
+    y.each { |ele|
+
+      @y_stt[@y_map[ele]] += 1
+
+    }
+
+    total_size = y.size
+
+    @y_prob = Array.new @y_map.size, 0
+
+    @y_stt.each_with_index { |val, idx|
+
+      @y_prob[idx] = val / total_size.to_f
+
+    }
+
+  end
+
+  def check_x x, y
+
+    row_size = x.size
+
+    column_size = x[0].size
+
+    build_x_map_tbls x
+
+    @x_stt_tbls = Array.new column_size
+
+    @x_prob_tbls = Array.new column_size
+
+    0.upto(column_size - 1) { |cidx|
+
+      x_map = @x_map_tbls[cidx]
+
+      x_stt_tbl = Array.new(@y_map.size)
+
+      x_stt_tbl.each_index {|idx| x_stt_tbl[idx] = Array.new(x_map.size, 0)}
+
+      0.upto(row_size - 1) { |ridx|
+
+        x_stt_tbl[@y_map[y[ridx]]][x_map[x[ridx][cidx]]] += 1
+
+      }
+
+      @x_stt_tbls[cidx] = x_stt_tbl
+
+      x_prob_tbl = Array.new(@y_map.size)
+
+      x_prob_tbl.each_index {|idx| x_prob_tbl[idx] = Array.new(x_map.size, 0)}
+
+      x_stt_tbl.each_with_index { |row_val, row|
+
+        row_val.each_with_index { |val, col|
+
+          x_prob_tbl[row][col] = val/@y_stt[row].to_f
+
+        }
+
+      }
+
+      @x_prob_tbls[cidx] = x_prob_tbl
+
+    }
+
+  end
+
   def fit x, y, discrete_spec
 
   end
@@ -9,4 +133,10 @@ class HybridBayesianClassifier
 
 
   end
+
+end
+
+
+if $0 == __FILE__
+
 end
