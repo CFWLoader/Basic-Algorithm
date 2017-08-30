e5ff9f0a9d5ad9453d13df6fe9ab7721a85b84bd
Evan Huang <cfwloader@gmail.com>
2017-08-30 16:55:06 +0800

    Debugging the algorithm.

--- Lovain/Louvain.hpp
+++ Lovain/Louvain.hpp
@@ -106,8 +106,30 @@ bool release_graph(vector<const Vertex *> graph)
     return true;
 }
 
-double modularity(set<const Vertex *> community)
+double modularity(set<const Vertex *> community, double half_total_weight)
 {
+
+    map<const Vertex*, double> k_values;
+
+    double q_value = 0;
+
+    for (set<const Vertex *>::const_iterator vertex_iter = community.begin();
+         vertex_iter != community.end();
+         ++vertex_iter)
+    {
+
+        k_values[*vertex_iter] = 0;
+
+        for (map<const Vertex *, double>::const_iterator neighbor_iter = ((*vertex_iter)->neighbors).begin();
+             neighbor_iter != ((*vertex_iter)->neighbors).end();
+             ++neighbor_iter)
+        {
+            k_values[*vertex_iter] += neighbor_iter->second;
+        }
+
+        // std::cout << (*vertex_iter)->node_num << ": " << k_values[*vertex_iter] << std::endl;
+    }
+
     for (set<const Vertex *>::const_iterator vertex = community.begin();
          vertex != community.end();
          ++vertex)
@@ -116,11 +138,93 @@ double modularity(set<const Vertex *> community)
              neighbor != ((*vertex)->neighbors).end();
              ++neighbor)
         {
+            if(community.find(neighbor->first) == community.end())
+            {
+                continue;
+            }
 
+            //std::cout << k_values[*vertex] * k_values[neighbor->first] << std::endl;
+
+            q_value += (neighbor->second - k_values[*vertex] * k_values[neighbor->first] / (2 * half_total_weight));
         }
     }
 
-    return 0;
+    return q_value / (2 * half_total_weight);
+}
+
+map<const Vertex*, set<const Vertex*>> louvain(vector<const Vertex*> graph)
+{
+    map<const Vertex*, set<const Vertex*>> partitions;
+
+    double total_weight = 0;
+
+    for(auto vertex : graph)
+    {
+        //cout << vertex->node_num << ":";
+        partitions[vertex] = set<const Vertex*>({vertex});
+
+        for(auto neighbor : vertex->neighbors)
+        {
+            // cout << neighbor.second << " ";
+
+            total_weight += neighbor.second;
+        }
+
+        // cout << endl;
+    }
+
+    double max_delta_q = 1, delta_q;
+
+    const Vertex* merging1, *merging2 ;
+
+    while(max_delta_q > 0)
+    {
+        max_delta_q = -1.1;
+
+        merging1 = merging2 = nullptr;
+
+        for(auto partition : partitions)
+        {
+            for(auto neighbor : partition.first->neighbors)
+            {
+                set<const Vertex*> new_set(partitions[neighbor.first].begin(), partitions[neighbor.first].end());
+
+                delta_q = modularity(new_set, total_weight / 2);
+
+                new_set.insert(partition.second.begin(), partition.second.end());
+
+                delta_q = modularity(new_set, total_weight / 2) - delta_q;
+
+                if(delta_q > max_delta_q)
+                {
+                    max_delta_q = delta_q;
+
+                    merging1 = partition.first;
+
+                    merging2 = neighbor.first;
+                }
+
+            }
+        }
+
+        if(max_delta_q > 0)
+        {
+            if(merging1 == nullptr or merging2 == nullptr)
+            {
+                throw "Bad Logic!";
+            }
+
+            std::cout << "Merging: " << merging1->node_num << " and " << merging2->node_num << std::endl;
+
+            partitions[merging1].insert(partitions[merging2].begin(), partitions[merging2].end());
+
+            partitions.erase(merging2);
+        }
+
+        // std::cout << partitions.size() << std::endl;
+    }
+
+    return partitions;
 }
 
 #endif //LOVAIN_LOUVAIN_HPP
