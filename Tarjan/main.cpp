ae7bf764a593b281c358a2986429a0e52ebc5d3e
Evan Huang <cfwloader@gmail.com>
2017-12-04 16:36:20 +0800

    Demo is available.

--- Tarjan/main.cpp
+++ Tarjan/main.cpp
@@ -3,6 +3,40 @@
 
 using namespace std;
 
+int** loadGraph(string filePath, int& size);
+
+int destructGraph(int** graph, const int size);
+
+void dfsEntry(int, int**, const int);
+
+void dfs(int u, int**, const int, int*, int*, int*, int* parent);
+
+int main()
+{
+    int size;
+
+    int** graph = loadGraph("sample0.txt", size);
+
+    for(int i = 0; i < size; ++i)
+    {
+        for(int j = i + 1; j < size; ++ j)
+        {
+            if(graph[i][j] != graph[j][i])
+            {
+                cout << "Not a symmetric matrix." << endl;
+
+                cout << "I: " << i << "  J: " << j << endl;
+            }
+        }
+    }
+
+    dfsEntry(0, graph, size);
+
+    destructGraph(graph, size);
+
+    return 0;
+}
+
 int** loadGraph(string filePath, int& size)
 {
     int src, dest;
@@ -16,6 +50,11 @@ int** loadGraph(string filePath, int& size)
     for(int idx = 0; idx < size; ++idx)
     {
         graph[idx] = new int[size];
+
+        for(int jIdx = 0; jIdx < size; ++jIdx)
+        {
+            graph[idx][jIdx] = 0;
+        }
     }
 
     while(!ifstream1.eof())
@@ -42,26 +81,68 @@ int destructGraph(int** graph, const int size)
     return 0;
 }
 
-int main()
+void dfsEntry(int u, int** graph, const int size)
 {
-    int size;
+    int* dfn = new int[size], *back = new int[size], *visted = new int[size], *parent = new int[size];
 
-    int** graph = loadGraph("sample1.txt", size);
-
-    for(int i = 0; i < size; ++i)
+    for(int idx = 0; idx < size; ++ idx)
     {
-        for(int j = i + 1; j < size; ++ j)
-        {
-            if(graph[i][j] != graph[j][i])
-            {
-                cout << "Not a symmetric matrix." << endl;
-
-                cout << "I: " << i << "  J: " << j << endl;
-            }
-        }
+        dfn[idx] = back[idx] = parent[idx] = -1;
+        visted[idx] = 0;
     }
 
-    destructGraph(graph, size);
+    dfs(u, graph, size, visted, dfn, back, parent);
 
-    return 0;
+    delete[] visted;
+
+    delete[] dfn;
+
+    delete[] parent;
+
+    delete[] back;
+}
+
+void dfs(int u, int** graph, const int size, int* visited, int* dfn, int* back, int* parent)
+{
+    static int counter = 0;
+
+    int children = 0;
+
+    ++counter;
+
+    dfn[u] = back[u] = counter;
+
+    visited[u] = 1;
+
+    for(int idx = 0; idx < size; ++idx)
+    {
+        if(u == idx or graph[u][idx] == 0)
+        {
+            continue;
+        }
+
+        if(!visited[idx])
+        {
+            ++children;
+
+            parent[idx] = u;
+
+            dfs(idx, graph, size, visited, dfn, back, parent);
+
+            back[u] = min(back[u], back[idx]);
+
+            if(parent[u] == -1 and children > 1)
+            {
+                cout << "Articulation Point: " << char('A' + u) << endl;
+            }
+            else if(parent[u] != -1 and back[idx] >= dfn[u])
+            {
+                cout << "Articulation Point: " << char('A' + u) << endl;
+            }
+        }
+        else if(idx != parent[u])
+        {
+            back[u] = min(back[u], dfn[idx]);
+        }
+    }
 }
